8142a9f20854029947eee0a4c8bd9faab7d3543c
cadamczyk <65303676+cadamczyk@users.noreply.github.com>
2025-02-17 13:36:38 -0500

    Add files via upload

---
+++ Intro_to_R_Answers.R
@@ -0,0 +1,54 @@
+#addition
+3+5
+
+#subtraction
+5-3
+
+#multiplication
+3*5
+
+#division
+15/3
+
+#exponent
+3^2
+
+#remainder
+28%%6
+
+#Assign the value 42 to x:
+x <- 42
+
+#print out the value of the variable x:
+print(x)
+
+#Assign the value 5 to the variable my_apples:
+my_apples <- 5
+
+#print out the value of the variable my_apple:
+print(my_apples)
+
+#Assign the value 6 to my_oranges:
+my_oranges <- 6
+
+#Add my_apples and my_oranges together:
+my_apples + my_oranges
+
+#Assign the result of adding my_apples and my_oranges to a 
+#new variable my_fruit:
+my_fruit <- my_apples + my_oranges
+print(my_fruit)
+
+#Assign 13 to a new variable my_numeric:
+my_numeric <- 13
+
+#Assign Brits to a new variable my_character:
+my_character <- "Brits"
+
+#Assign FALSE to a new variable my_logical:
+my_logical <- FALSE
+
+#Check the class of my_numeric, my_character, and my_logical:
+class(my_numeric)
+class(my_character)
+class(my_logical)

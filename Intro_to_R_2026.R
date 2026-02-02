8a927413bce386ad9e0186602409c52cb7fbc346
cadamczyk <65303676+cadamczyk@users.noreply.github.com>
2026-02-02 13:06:10 -0500

    Add files via upload

---
+++ Intro_to_R_2026.R
@@ -0,0 +1,50 @@
+#addition
+
+
+#subtraction
+
+
+#multiplication
+
+
+#division
+
+
+#exponent
+
+
+#remainder
+
+
+#Assign the value 42 to x:
+
+
+#print out the value of the variable x:
+
+
+#Assign the value 5 to the variable my_apples:
+
+
+#print out the value of the variable my_apple:
+
+
+#Assign the value 6 to my_oranges:
+
+
+#Add my_apples and my_oranges together:
+
+
+#Assign the result of adding my_apples and my_oranges 
+#to a new variable my_fruit:
+
+
+#Assign 13 to a new variable my_numeric:
+
+
+#Assign Brits to a new variable my_character:
+
+
+#Assign FALSE to a new variable my_logical:
+
+
+#Check the class of my_numeric, my_character, and my_logical:

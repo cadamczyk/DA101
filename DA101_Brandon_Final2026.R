921cdebb086583e7c3173ebdc22423ab823dda7a
cadamczyk <65303676+cadamczyk@users.noreply.github.com>
2026-04-22 11:37:05 -0400

    Add files via upload

---
+++ DA101_Brandon_Final2026.R
@@ -0,0 +1,34 @@
+install.packages("tidyverse")
+library(tidyverse)
+install.packages("dplyr")
+library(dplyr)
+install.packages("ggplot2")
+library(ggplot2)
+install.packages("coorplot")
+library(corrplot)
+
+#upload built in dataset "mtcars"
+
+
+#calculate the median horsepower and weight
+
+
+#How many cars have more than 6 cylinders?
+
+
+#How many cars have more than 100 horsepower (hp) and weigh (column wt) less than 3,000 lbs?
+
+
+#Calculate the mean mpg by cylinder type
+
+
+#Draw a scatter plot using ggplot showing the relationship between wt and mpg.
+
+
+#Draw a scatter plot using ggplot showing the relationship between hp and mpg.
+
+
+#create a correlation plot
+
+
+#build a linear regression model between mpg as outcome and wt and hp as predictors
